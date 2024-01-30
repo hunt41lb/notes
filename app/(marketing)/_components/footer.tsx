b762758e140d815e7994760ee68e19f974b0a1af
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2024-01-30 14:17:08 -0500

    Update footer.tsx

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -4,20 +4,20 @@ import { Logo } from "./logo"
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#030712]">
+    <div className="flex items-center w-full p-6 bg-background z-50">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <a href="https://github.com/hunt41lb/Notes" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/hunt41lb/Notes" target="_blank">
           <Button variant="ghost" size="sm">
             Github
           </Button>
         </a>
-        <a href="https://ndit.services" target="_blank" rel="noopener noreferrer">
+        <a href="https://ndit.services/" target="_blank">
           <Button variant="ghost" size="sm">
-            Home
+            Other Applications
           </Button>
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
