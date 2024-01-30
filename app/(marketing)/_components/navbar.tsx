0c39e8d6db2f29294e3f4e805f10bbe55e76777e
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2024-01-30 14:17:52 -0500

    Update navbar.tsx

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+// Marketing Components //
+import { Logo } from "./logo";
+
+// Hooks //
+import { useScrollTop } from "@/hooks/use-scroll-top";
+
+// Components //
+import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import Link from "next/link";
-
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
-import { cn } from "@/lib/utils";
 
-import { Logo } from "./logo";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -18,8 +22,8 @@ export const Navbar = () => {
 
   return (
     <div className={cn(
-      "z-50 bg-background dark:bg-[#030712] fixed top-0 flex items-center w-full p-6",
-      scrolled && "border-b shadow-sm"
+      "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+      scrolled && "bored-b shadow-sm"
     )}>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
@@ -29,20 +33,20 @@ export const Navbar = () => {
         {!isAuthenticated && !isLoading && (
           <>
             <SignInButton mode="modal">
-              <Button variant="ghost" size="sm">
+              <Button variant="secondary" size="sm" className="text-foreground">
                 Sign In
               </Button>
             </SignInButton>
             <SignInButton mode="modal">
               <Button size="sm">
-                Get Notes
+                Get Notes Free
               </Button>
             </SignInButton>
           </>
         )}
         {isAuthenticated && !isLoading && (
           <>
-            <Button variant="ghost" size="sm" asChild>
+            <Button size="sm" asChild>
               <Link href="/documents">
                 View Notes
               </Link>
