e5df718e505f444739217249897a0349fc4e6a62
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2024-01-30 14:21:39 -0500

    Update navbar.tsx

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -11,6 +11,7 @@ import { Title } from "./title";
 import { Banner } from "./banner";
 import { Menu } from "./menu";
 import { Publish } from "./publish";
+import { NavbarDropdown } from "./navbardropdown";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -22,14 +23,14 @@ export const Navbar = ({
   onResetWidth
 }: NavbarProps) => {
   const params = useParams();
-
+ 
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId as Id<"documents">,
   });
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#030712] px-3 py-2 w-full flex items-center justify-between">
+      <nav className="bg-background dark:bg-background px-3 py-2 w-full flex items-center text-foreground justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
@@ -44,7 +45,7 @@ export const Navbar = ({
 
   return (
     <>
-      <nav className="bg-background dark:bg-[#030712] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-background border-b border-border px-2 py-2 h-12 w-full flex items-center">
         {isCollapsed && (
           <MenuIcon
             role="button"
@@ -52,12 +53,15 @@ export const Navbar = ({
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between w-full">
+        <div className="flex-1 flex items-center justify-start">
+          <NavbarDropdown />
+        </div>
+        <div className="flex-1 flex items-center text-center justify-center">
           <Title initialData={document} />
-          <div className="flex items-center gap-x-2">
-            <Publish initialData={document} />
-            <Menu documentId={document._id} />
-          </div>
+        </div>
+        <div className="flex-1 flex items-center justify-end px-4 gap-x-2 text-foreground">
+          <Publish initialData={document} />
+          <Menu documentId={document.id} />
         </div>
       </nav>
       {document.isArchived && (
