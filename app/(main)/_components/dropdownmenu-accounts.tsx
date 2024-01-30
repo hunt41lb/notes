79e56e502f27f07b2bfc75a39a576395e1e908f3
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2024-01-30 14:19:35 -0500

    Create dropdownmenu-accounts.tsx

---
+++ app/(main)/_components/dropdownmenu-accounts.tsx
@@ -0,0 +1,135 @@
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  ShieldEllipsis,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+import { 
+  useUser,
+  UserProfile,
+  SignOutButton,
+} from "@clerk/clerk-react";
+
+import { ModeToggle } from "@/components/mode-toggle";
+import { useSettings } from "@/hooks/use-settings";
+
+import {
+  Avatar,
+  AvatarImage,
+} from "@/components/ui/avatar";
+
+import { AccountManagement } from "./account-management";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function DropdownMenuAccount() {
+  const { user } = useUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">Account</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="text-xs w-60">
+        <DropdownMenuLabel>{user?.firstName}&apos;s Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Avatar className="mr-2 h-4 w-4">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+              <span>Manage Account</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem className="rounded-sm">
+                  <Avatar className="mr-2 h-4 w-4">
+                    <AvatarImage src={user?.imageUrl} />
+                  </Avatar>
+                  <span className="text-start w-60 text-sm text-foreground font-medium line-clamp-1">
+                    {user?.fullName}
+                  </span>
+                </DropdownMenuItem> 
+               <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Mail className="mr-2 h-4 w-4" />
+                  <p className="text-sm font-medium leading-none text-muted-foreground">
+                    {user?.emailAddresses[0].emailAddress}
+                  </p>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>
+                  <ModeToggle />
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            <Keyboard className="mr-2 h-4 w-4" />
+            <span>Keyboard shortcuts</span>
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <a href="https://github.com/hunt41lb/Notes" target="_blank" rel="noopener noreferrer" className="flex items-center">
+            <Github className="mr-2 h-4 w-4" />
+            <span>GitHub</span>
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <a href="https://lookitup.io" target="_blank" rel="noopener noreferrer" className="flex items-center">
+            <LifeBuoy className="mr-2 h-4 w-4" />
+            <span>Support</span>
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled>
+          <Cloud className="mr-2 h-4 w-4" />
+          <span>API</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+            <SignOutButton>
+              Sign Out
+            </SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
