701fa044d0351b629b2987807cc89778970fb87c
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2024-01-30 14:16:12 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import { Toaster } from "sonner";
-import { Inter } from 'next/font/google'
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
+import { Toaster } from "sonner";
+
+// Providers //
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
 
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Notes',
-  description: 'Network Down IT - Notes',
+  title: "Notes",
+  description: "Network Down IT - Notes Application",
   icons: {
     icon: [
       {
@@ -28,7 +29,7 @@ export const metadata: Metadata = {
       }
     ]
   }
-}
+};
 
 export default function RootLayout({
   children,
@@ -45,7 +46,7 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              storageKey="notes-theme"
+              storageKey="default"
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
